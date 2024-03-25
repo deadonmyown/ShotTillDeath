1597c85840d04001e2945c67f58dafd90dd35453
Зотов Антон Александрович <AntAZotov@int.kpfu.ru>
2024-03-25 15:28:34 +0300

    start interaction system implementation

--- Source/ShotTillDeath/Private/InteractionQueueComponent.cpp
+++ Source/ShotTillDeath/Private/InteractionQueueComponent.cpp
@@ -1,34 +1,43 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "InteractionQueueComponent.h"
 
-// Sets default values for this component's properties
 UInteractionQueueComponent::UInteractionQueueComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
 }
 
 
-// Called when the game starts
 void UInteractionQueueComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
 	
 }
 
 
-// Called every frame
 void UInteractionQueueComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+}
+
+bool UInteractionQueueComponent::Add(AActor* Actor, const FInteractionData& InteractionData)
+{
+}
+
+bool UInteractionQueueComponent::Remove(const AActor* Actor)
+{
+}
+
+bool UInteractionQueueComponent::StartInteraction()
+{
+}
+
+bool UInteractionQueueComponent::FinishInteraction(AActor* Actor)
+{
+}
+
+bool UInteractionQueueComponent::StopInteraction()
+{
 }
 
--- Source/ShotTillDeath/Public/InteractionQueueComponent.h
+++ Source/ShotTillDeath/Public/InteractionQueueComponent.h
@@ -6,6 +6,44 @@
 #include "Components/ActorComponent.h"
 #include "InteractionQueueComponent.generated.h"
 
+USTRUCT(BlueprintType)
+struct FInteractionData
+{
+	GENERATED_BODY()
+
+	/**
+	 * Toggles if the actor require being in the line of sight to be interacted. 
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="InteractionData")
+	bool bRequireLineOfSight = false;
+
+	/**
+	 * A message which can be used in HUD.
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="InteractionData")
+	FString InteractionMessage = "Interact";
+
+	/**
+	 * How much time required to activate interaction effect.
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="InteractionData", meta=(ClampMin="0"))
+	float InteractionTime = 0.f;
+};
+
+USTRUCT(BlueprintType)
+struct FQueueData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="QueueData")
+	AActor* Actor = nullptr;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="QueueData")
+	FInteractionData InteractionData;
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnInteraction, AActor*, TargetActor);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnQueueChanged, const AActor*, OtherActor);
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class SHOTTILLDEATH_API UInteractionQueueComponent : public UActorComponent
@@ -25,13 +63,54 @@ public:
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 
-	UFUNCTION()
-	void StartInteraction(AActor* OtherActor);
+	/** Called when the interaction process started. */
+	UPROPERTY(BlueprintAssignable, Category="Interaction")
+	FOnInteraction OnInteractionStarted;
 
-	UFUNCTION()
-	bool FinishInteraction(AActor* OtherActor);
+	/** Called when the interaction effect successfully activated. */
+	UPROPERTY(BlueprintAssignable, Category="Interaction")
+	FOnInteraction OnInteractionFinished;
 
+	/** Called when the interaction process stopped. */
+	UPROPERTY(BlueprintAssignable, Category="Interaction")
+	FOnInteraction OnInteractionStopped;
+
+	/** Called when actor was added to the queue. */
+	UPROPERTY(BlueprintAssignable, Category="Interaction")
+	FOnQueueChanged OnActorAdded;
+
+	/** Called when actor was added to the queue. */
+	UPROPERTY(BlueprintAssignable, Category="Interaction")
+	FOnQueueChanged OnActorRemoved;
+
+	/** Adds interaction data into the interaction queue. */
+	UFUNCTION(BlueprintCallable, Category="InteractionSystem")
+	bool Add(AActor* Actor, const FInteractionData& InteractionData);
+
+	/** Removes interaction data into the interaction queue. */
+	UFUNCTION(BlueprintCallable, Category="InteractionSystem")
+	bool Remove(const AActor* Actor);
+
+	/** Starts interaction with the first actor in the interaction queue. */
+	UFUNCTION(BlueprintCallable, Category="InteractionSystem")
+	bool StartInteraction();
+
+	/** Finishes interaction.
+	 * Call it if FinishManually == false.
+	 */
 	UFUNCTION()
-	void StopInteraction(AActor* OtherActor);
+	bool FinishInteraction(AActor* Actor);
+
+	/** Stops interaction.*/
+	UFUNCTION(BlueprintCallable, Category="InteractionSystem")
+	bool StopInteraction();
+
+private:
+	/** If true, the FinishInteraction() must be called manually.*/
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Interaction", meta=(AllowPrivateAccess))
+	bool bFinishManually = false;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Interaction", meta=(AllowPrivateAccess))
+	TArray<FQueueData> InteractionQueue;
 		
 };
